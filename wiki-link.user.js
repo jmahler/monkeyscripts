99b927cf1555230adca659dd210c47429b35d930
Jeremiah Mahler <jmmahler@gmail.com>
2016-12-06 21:58:52 -0800

    version 0.3

--- wiki-link.user.js
+++ wiki-link.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Wiki heading link
-// @version      0.2
+// @version      0.3
 // @description  Create an easy to copy section heading link.
 // @updateURL    https://github.com/jmahler/monkeyscripts/raw/master/wiki-link.js
 // @downloadURL  https://github.com/jmahler/monkeyscripts/raw/master/wiki-link.js
